5c1ea2ce40b2fa49f8660a3bc4dd0bd23dda626a
Bryn Deering <bdeering@uoguelph.ca>
2020-11-19 23:58:55 -0500

    added variables to board.js

--- js/board.js
+++ js/board.js
@@ -1,3 +1,7 @@
+function PIECEINDEX(piece, pieceNum) {
+    return (piece * 10 + pieceNUm);
+}
+
 var GameBoard = {};
 
 GameBoard.pieces = new Array(BRD_SQ_NUM);
@@ -5,4 +9,9 @@ GameBoard.side = new Array(COLOUR.WHITE);
 GameBoard.fifityMove = 0;
 GameBoard.plyNum = 0;
 GameBoard.ply = 0;
+GameBoard.enPas = 0;
 GameBoard.castlePerm = 0;
+GameBoard.material = new Array(2); /*white, black material total*/
+GameBoard.numPieces = new Array(13); /*indexed by PIECES*/
+GameBoard.pList = new Array(14 * 10); /*list of number of each type of pieces for each side*/
+GameBoard.posKey = 0; /*unique key for each board position*/
