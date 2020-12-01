cb7d3a275df73b12dff2e757e5e2cd6dfe9d1218
Bryn Deering <bdeering@uoguelph.ca>
2020-11-30 23:12:46 -0500

    Updated GUI

--- js/gui.js
+++ js/gui.js
@@ -3,3 +3,13 @@ $("#SetFen").click(function() {
     ParseFen(fenStr);
     PrintBoard();
 });
+
+$("h3").click(function() {
+    $("h3").hide();
+    $("#BoardDiv").slideToggle();
+});
+
+$("#BoardDiv").click(function() {
+    $("h3").show();
+    $("#BoardDiv").fadeToggle();
+});
