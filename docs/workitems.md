1ca7489e2911a78339050959d235b16c6fdf603a
Bryn Deering <bdeering@uoguelph.ca>
2022-04-28 23:48:18 -0400

    Updated work items and some misc cleanup

--- docs/workitems.md
+++ docs/workitems.md
@@ -1,8 +1,14 @@
 ## Work Items
 
+### Next Steps
 - test documentation
   - document existing test cases in testing.md
   - make a testing plan which includes missing test cases and revisions
 - test refactoring
   - make nevessary changes to unit tests according to testing plan
   - consider implementing a testing framework (eg. Jest)
+
+### Technical Debt
+- markup and module structure
+  - combine debug.html and index.html into one file
+  - implement a better module system (ESM modules?)
--- src/main.js
+++ src/main.js
@@ -4,17 +4,15 @@ $(document).ready(function() {
 });
 
 function InitFilesRanksBrd() {
-    var file = FILES.FILE_A;
-    var rank = RANKS.RANK_1;
     var sq = SQUARES.A1;
     
-    for (i= 0; i < BRD_SQ_NUM; i++) {
+    for (let i = 0; i < BRD_SQ_NUM; i++) {
         FilesBoard[i] = SQUARES.OFFBOARD;
         RanksBoard[i] = SQUARES.OFFBOARD;
     }
     
-    for (rank = RANKS.RANK_1; rank <= RANKS.RANK_8; rank++) {
-        for (file = FILES.FILE_A; file <= FILES.FILE_H; file++) {
+    for (let rank = RANKS.RANK_1; rank <= RANKS.RANK_8; rank++) {
+        for (let file = FILES.FILE_A; file <= FILES.FILE_H; file++) {
             sq = FR2SQ(file,rank);
             FilesBoard[sq] = file;
             RanksBoard[sq] = rank;
@@ -23,33 +21,31 @@ function InitFilesRanksBrd() {
 }
 
 function InitHashKeys() {
-    for (var i = 0; i < 13 * 120; i++) {
+    for (let i = 0; i < 13 * 120; i++) {
         PieceKeys[i] = RAND_32();
     }
 
     SideKey = RAND_32(); /* hashed in if white is to move*/
     
-    for (var i = 0; i < 16; i++) {
+    for (let i = 0; i < 16; i++) {
         CastleKeys[i] = RAND_32();
     }
 }
 
 function InitSq120ToSq64() { /*this could probably be done better*/ 
-    var file = FILES.FILE_A;
-    var rank = RANKS.RANK_1;
     var sq = SQUARES.A1;
     var sq64 = 0;
     
-    for (var i = 0; i < BRD_SQ_NUM; i++) {
+    for (let i = 0; i < BRD_SQ_NUM; i++) {
         Sq120ToSq64[i] = 120;
     }
     
-    for (var i = 0; i < 64; i++) {
+    for (let i = 0; i < 64; i++) {
         Sq64ToSq120[i] = 65;
     }
     
-    for (rank = RANKS.RANK_8; rank >= RANKS.RANK_1; rank--) {
-        for (file = FILES.FILE_A; file <= FILES.FILE_H; file++) {
+    for (let rank = RANKS.RANK_8; rank >= RANKS.RANK_1; rank--) {
+        for (let file = FILES.FILE_A; file <= FILES.FILE_H; file++) {
             sq = FR2SQ(file,rank);
             Sq120ToSq64[sq] = sq64;
             Sq64ToSq120[sq64] = sq;
